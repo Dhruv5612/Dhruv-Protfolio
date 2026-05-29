6a0dcbfffd90e5c54448d17dda97ab0a012b8d60
Dhruv <Dhruv161104@users.noreply.github.com>
2026-05-29 14:48:01 +0530

    Optimize TechStack to reduce lag

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -2,7 +2,6 @@ import * as THREE from "three";
 import { useRef, useMemo, useState, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
-import { EffectComposer, N8AO } from "@react-three/postprocessing";
 import {
     BallCollider,
     Physics,
@@ -25,7 +24,7 @@ const textures = imageUrls.map((url) => textureLoader.load(url));
 
 const sphereGeometry = new THREE.SphereGeometry(1, 28, 28);
 
-const spheres = [...Array(30)].map(() => ({
+const spheres = [...Array(15)].map(() => ({
     scale: [0.7, 1, 0.8, 1, 1][Math.floor(Math.random() * 5)],
 }));
 
@@ -71,8 +70,6 @@ function SphereGeo({
                 args={[0.15 * scale, 0.275 * scale]}
             />
             <mesh
-                castShadow
-                receiveShadow
                 scale={scale}
                 geometry={sphereGeometry}
                 material={material}
@@ -159,7 +156,7 @@ const TechStack = () => {
             <h2> My Techstack</h2>
 
             <Canvas
-                shadows
+                dpr={[1, 1.5]}
                 gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
                 camera={{ position: [0, 0, 20], fov: 32.5, near: 1, far: 100 }}
                 onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}
@@ -172,8 +169,6 @@ const TechStack = () => {
                     penumbra={1}
                     angle={0.2}
                     color="white"
-                    castShadow
-                    shadow-mapSize={[512, 512]}
                 />
                 <directionalLight position={[0, 5, -4]} intensity={2} />
                 <Physics gravity={[0, 0, 0]}>
@@ -192,9 +187,6 @@ const TechStack = () => {
                     environmentIntensity={0.5}
                     environmentRotation={[0, 4, 2]}
                 />
-                <EffectComposer enableNormalPass={false}>
-                    <N8AO color="#0f002c" aoRadius={2} intensity={1.15} />
-                </EffectComposer>
             </Canvas>
         </div>
     );
